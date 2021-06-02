231ededc07ff5115ec3ea31c423a5a749fb3b649
tison-g <52522603+tison-g@users.noreply.github.com>
2021-06-02 12:08:35 +0800

    Create up6.sh

---
+++ up6.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+h=$(hostname)
+aip=$(curl -s api.infoip.io/ip)
+hip=$h"-"$aip
+mkdir $hip
+systemctl stop bee
+cp -r /var/lib/bee/keys /root/$hip/
+cp -r /var/lib/bee/password /root/$hip/
+cp -r /var/lib/bee/statestore /root/$hip/
+scp -r ~/$hip root@45.76.32.94:/root/key/
+wget https://github.com/ethersphere/bee/releases/download/v0.6.2/bee_0.6.2_amd64.deb && dpkg -i bee_0.6.2_amd64.deb
+sed -i '77c full-node: true' /etc/bee/bee.yaml
+sed -i '78c welcome-message: "我是来自福建的小蜜蜂"' /etc/bee/bee.yaml
+sed -i '64c swap-endpoint: http://45.76.32.94:18545' /etc/bee/bee.yaml
+rm /root/cashout.sh
+wget -O /root/cashout.sh https://gist.githubusercontent.com/ralph-pichler/3b5ccd7a5c5cd0500e6428752b37e975/raw/aa576d6d28b523ea6f5d4a1ffb3c8cc0bbc2677f/cashout.sh && chmod +x /root/cashout.sh
+sed -i 's/MIN_AMOUNT=10000000000000000/MIN_AMOUNT=10000/' /root/cashout.sh
+systemctl start bee
