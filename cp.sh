76e91e3358f1295c726839d30ac9efd276ac5035
tison-g <52522603+tison-g@users.noreply.github.com>
2021-06-17 16:00:15 +0800

    Update cp.sh

--- cp.sh
+++ cp.sh
@@ -9,4 +9,4 @@ cp -r /var/lib/bee/keys /root/$hip/
 cp -r /var/lib/bee/password /root/$hip/
 cp -r /var/lib/bee/statestore /root/$hip/
 cp -r /var/lib/bee-clef /root/$hip/
-scp -r /root/$hip root@23.226.1.27:/root/key/
+scp -r /root/$hip root@149.28.153.88:/root/key/
